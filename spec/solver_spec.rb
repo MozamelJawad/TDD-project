4d848e8f100ac2bec3b454a9477f738bec19d4e9
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-20 10:11:31 +0430

    Create solver_spec.rb file and create test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'solver'
+
+describe 'During testing "Solver" class' do
+
+  before(:each) do
+    @solver =  Solver.new
+  end
+
+# test for factorial method
+  describe 'When testing "factorial" method' do
+    it 'returns 1 for factorial of 0' do
+      expect(@solver.factorial(0)).to eql(1)
+    end
+
+    it 'returns the correct factorial for positive integers' do
+      expect(@solver.factorial(5)).to eql(120)
+      expect(@solver.factorial(6)).to eql(720)
+      expect(@solver.factorial(10)).to eql(3_628_800)
+    end
+
+    it 'raises an exception for negative integers' do
+      expect { @solver.factorial(-5) }.to raise_error(ArgumentError)
+    end
+  end
+end
